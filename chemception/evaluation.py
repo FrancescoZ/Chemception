d46b7ed044cb68735fe4e310f8110b49b950f312
Francesco Zanoli <francesco.zanoli@gmail.com>
2018-05-16 18:27:31 +0200

    Minory changes

--- chemception/evaluation.py
+++ chemception/evaluation.py
@@ -11,15 +11,18 @@ class Metrics(Callback):
 		self.val_mccs = []
 	
 	def on_epoch_end(self, epoch, logs={}):
-		val_predict = (np.asarray(self.model.predict(self.validation_data[0]))).round()
-		val_targ = self.validation_data[1]
+		try:
+			val_predict = (np.asarray(self.model.predict(self.validation_data[0]))).round()
+			val_targ = self.validation_data[1]
 
-		_val_precision,_val_recall, _val_f1,supp  = precision_recall_fscore_support(val_targ, val_predict,average='samples')
-		#_val_mcc = matthews_corrcoef(val_targ, val_predict)
+			_val_precision,_val_recall, _val_f1,supp  = precision_recall_fscore_support(val_targ, val_predict,average='samples')
+			#_val_mcc = matthews_corrcoef(val_targ, val_predict)
 
-		self.val_f1s.append(_val_f1)
-		self.val_recalls.append(_val_recall)
-		self.val_precisions.append(_val_precision)
-		#self.val_mccs.append(_val_mcc)
-		print('- val_f1: %f - val_precision: %f - val_recall %f'%( _val_f1, _val_precision, _val_recall))
-		return
+			self.val_f1s.append(_val_f1)
+			self.val_recalls.append(_val_recall)
+			self.val_precisions.append(_val_precision)
+			#self.val_mccs.append(_val_mcc)
+			print('- val_f1: %f - val_precision: %f - val_recall %f'%( _val_f1, _val_precision, _val_recall))
+			return
+		except ValueError as e
+			print(e)
